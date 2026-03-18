65f3a8b9c3817f754124fde453c543b27580de4a
kashish0622 <kashishbhattofficial@gmail.com>
2026-03-18 16:22:24 +0530

    util.py file added for assignment3

--- src/Assignment3/util.py
+++ src/Assignment3/util.py
@@ -1,15 +1,16 @@
+from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
-from pyspark.sql.functions import to_date, col, current_date, date_sub, count
+from pyspark.sql.functions import *
 
-# Step 1: Create DataFrame
-def create_df(spark):
+def get_spark(name):
+    return SparkSession.builder.appName(name).getOrCreate()
+def get_df(spark):
     schema = StructType([
         StructField("log id", IntegerType(), True),
         StructField("user$id", IntegerType(), True),
         StructField("action", StringType(), True),
         StructField("timestamp", StringType(), True)
     ])
-
     data = [
         (1, 101, 'login', '2023-09-05 08:30:00'),
         (2, 102, 'click', '2023-09-06 12:45:00'),
@@ -20,20 +21,23 @@ def create_df(spark):
         (7, 103, 'click', '2023-09-11 10:15:00'),
         (8, 102, 'click', '2023-09-12 13:10:00')
     ]
-
     return spark.createDataFrame(data, schema)
 
-def rename_columns(df):
-    return df.toDF("log_id", "user_id", "user_activity", "time_stamp")
+def rename_cols(df):
+    new_cols = ["log_id", "user_id", "user_activity", "time_stamp"]
+    return df.toDF(*new_cols)
+
+def last_7_days(df):
+    df = df.withColumn("time_stamp", to_timestamp(col("time_stamp")))
+    return df.filter(col("time_stamp") >= date_sub(current_date(), 7)) \
+        .groupBy("user_id") \
+        .agg(count("*").alias("actions"))
 
 def add_login_date(df):
     return df.withColumn("login_date", to_date(col("time_stamp")))
 
-def get_user_actions(df):
-    filtered_df = df.filter(
-        col("login_date") >= date_sub(current_date(), 7)
-    )
+def write_csv(df, path):
+    df.write.mode("overwrite").option("header", True).option("delimiter", ",").csv(path)
 
-    return filtered_df.groupBy("user_id").agg(
-        count("user_activity").alias("action_count")
-    )
+def write_table(df):
+    df.write.mode("overwrite").saveAsTable("user.login_details")
